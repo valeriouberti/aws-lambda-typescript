01888bc182aa7162c3dd87a4951acd4e31681b9d
valeriouberti <valerio.uberti23@gmail.com>
2022-04-15 10:52:48 +0200

    Add person interface and dysplay it

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda/trigger/api-gateway-proxy";
 
+interface Person{
+  name:string
+  surname: string
+  age: number
+}
 
 export const lambdaHandler = async (
     event: APIGatewayProxyEvent
   ): Promise<APIGatewayProxyResult> => {
-    const queries = JSON.stringify(event.queryStringParameters);
+    const person: Person = JSON.parse(event.body);
     return {
       statusCode: 200,
-      body: `Queries: ${queries}`
+      body: JSON.stringify({'Person' : person})
     }
   }
